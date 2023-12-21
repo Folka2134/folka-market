bb288e70d92cd5548e2b186f1f2bd9808e97ecbb
Folka2134 <folka2134@gmail.com>
2023-12-21 19:44:32 +0000

    implemented clerk

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,8 @@
+import { SignIn } from "@clerk/nextjs";
 import React from "react";
 
 const Page = () => {
-  return <div>Page</div>;
+  return <SignIn />;
 };
 
 export default Page;
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,8 @@
+import { SignUp } from "@clerk/nextjs";
 import React from "react";
 
 const Page = () => {
-  return <div>Page</div>;
+  return <SignUp />;
 };
 
 export default Page;
