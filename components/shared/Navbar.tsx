6863d1717e949e27d2c79e9160606b2497c90951
Folka2134 <folka2134@gmail.com>
2024-01-04 22:12:33 +0000

    Created navbar

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,29 +1,35 @@
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 const Navbar = () => {
   return (
     <header className="w-full border-b">
-      <div className="wrapper flex items-center justify-between">
-        <Link href="/" className="w-36">
-          <p>Home</p>
-        </Link>
-
-        {/* <SignedIn>
-          <nav className="md:flex-between hidden w-full max-w-xs">
-            <NavItems />
-          </nav>
-        </SignedIn> */}
-
+      <div className="wrapper flex items-center justify-between w-full h-14">
+        <DropdownMenu>
+          <DropdownMenuTrigger>My Account</DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Saved items</DropdownMenuItem>
+            <DropdownMenuItem>Watch List</DropdownMenuItem>
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
-            {/* <MobileNav /> */}
           </SignedIn>
           <SignedOut>
             <button className="rounded-full">
-              <Link href="/sign-in">Login</Link>
+              <Link href="/sign-in">Signin/Signup</Link>
             </button>
           </SignedOut>
         </div>
