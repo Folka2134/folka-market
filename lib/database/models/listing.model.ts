16e8e5e105442ac93f98fa23875e1beebbaeb52f
Folka2134 <folka2134@gmail.com>
2024-01-05 12:32:56 +0000

    created listing schema

---
+++ lib/database/models/listing.model.ts
@@ -0,0 +1,29 @@
+import { Schema, model, models, Document } from "mongoose";
+
+export interface IListing extends Document {
+  _id: string
+  title: string,
+  condition: string,
+  location: string;
+  console: string,
+  price: number,
+  datePosted: Date,
+  category: { _id: string, name: string}[],
+  user: { _id: string, firstName: string, lastName: string },
+}
+
+
+const ListingSchema = new Schema({
+  title: { type: String, required: true },
+  condition: { type: String, required: true },
+  location: { type: String, required: true },
+  console: { type: String, required: true },
+  price: { type: Number, required: true },
+  datePosted: { type: Date, default: Date.now },
+  category: { type: Schema.Types.ObjectId, ref: 'Category' },
+  user: { type: Schema.Types.ObjectId, ref: 'User' },
+})
+
+const Listing = models.Listing || model('Listing', ListingSchema);
+
+export default Listing;
