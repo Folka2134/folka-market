a475c0df197d2113a2842f8c0caeda61a14bb965
Folka2134 <folka2134@gmail.com>
2024-01-07 13:40:10 +0000

    Added home link

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,18 +2,21 @@ import React from "react";
 import Navbar from "./Navbar";
 import Image from "next/image";
 import SearchSection from "./SearchSection";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div className="">
       <Navbar />
       <div className="flex w-full items-center">
-        <Image
-          src="/assets/images/Folka-logos_transparentsmall.png"
-          alt="Folka Logo"
-          height={150}
-          width={150}
-        />
+        <Link href="/">
+          <Image
+            src="/assets/images/Folka-logos_transparentsmall.png"
+            alt="Folka Logo"
+            height={150}
+            width={150}
+          />
+        </Link>
         <SearchSection />
       </div>
     </div>
