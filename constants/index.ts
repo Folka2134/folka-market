eb60be5fdf588dc82cd252c4f7417eb690f01345
Folka2134 <folka2134@gmail.com>
2024-01-07 13:39:13 +0000

    Added dropdown values

--- constants/index.ts
+++ constants/index.ts
@@ -15,11 +15,65 @@ export const CategoryList = [
 
 export const defaultListingValues = {
   id: "",
+  imageUrl: "",
 	title: "",
 	condition: "",
 	location: "",
+	category: "",
 	console: "",
 	price: "",
-	categoryId: "",
   datePosted: new Date(),
 }
+
+export const categories = {
+  Categories: [
+    "Action",
+    "Adventure",
+    "Role-Playing Game (RPG)",
+    "Simulation",
+    "Strategy",
+    "Sports",
+    "Fighting",
+    "Horror",
+    "Puzzle",
+    "Music/Rhythm",
+    "Party/Multiplayer",
+    "Simulation Racing",
+    "First-Person Shooter (FPS)",
+    "Third-Person Shooter (TPS)",
+    "Massively Multiplayer Online (MMO)",
+    "Battle Royale",
+    "Survival",
+    "Real-Time Strategy (RTS)",
+    "Turn-Based Strategy (TBS)",
+    "Educational"
+  ]
+}
+export const conditions = {
+  Conditions: [
+    "New",
+    "Used"
+  ]
+}
+
+export const consoles = {
+  Consoles: [
+    "PC",
+    "PlayStation 5",
+    "Xbox Series X",
+    "Nintendo Switch",
+    "PlayStation 4",
+    "Xbox One",
+    "Nintendo Wii U",
+    "Nintendo 3DS",
+    "PlayStation Vita",
+    "Xbox 360",
+    "PlayStation 3",
+    "Nintendo Wii",
+    "Xbox",
+    "PlayStation 2",
+    "Nintendo GameCube",
+    "Dreamcast"
+  ]
+}
+
