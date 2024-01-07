01d7bc15067d95806b3d2e1a1e7b6289c13d5639
Folka2134 <folka2134@gmail.com>
2024-01-07 13:42:10 +0000

    created create listing page

---
+++ app/(root)/listings/create/page.tsx
@@ -0,0 +1,23 @@
+import ListingForm from "@/components/shared/ListingForm";
+import { auth } from "@clerk/nextjs";
+import React from "react";
+
+const CreateListingPage = () => {
+  const { sessionClaims } = auth();
+
+  const userId = sessionClaims?.userId as string;
+
+  return (
+    <>
+      <section>
+        <h1 className="text-center my-10 font-bold text-2xl">Create Listing</h1>
+
+        <div className="wrapper">
+          <ListingForm userId={userId} type="create" />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default CreateListingPage;
