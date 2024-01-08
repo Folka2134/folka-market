de89ced7b8ef2806c47fa897be98ab0758bf06a9
Folka2134 <folka2134@gmail.com>
2024-01-08 14:47:04 +0000

    added profile link

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,4 +1,4 @@
-import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton, auth } from "@clerk/nextjs";
 import Link from "next/link";
 import React from "react";
 import {
@@ -10,22 +10,30 @@ import {
 import { Button } from "../ui/button";
 
 const Navbar = () => {
+  const { sessionClaims } = auth();
+
+  const userId = sessionClaims?.userId as string;
+
   return (
     <header className="w-full border-b">
       <div className="flex items-center justify-between w-full h-14">
         <div className="flex gap-5">
-          <DropdownMenu>
-            <DropdownMenuTrigger>My Account</DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Saved items</DropdownMenuItem>
-              <DropdownMenuItem>Watch List</DropdownMenuItem>
-              <DropdownMenuItem>Settings</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-          <Button className="bg-pink-400">
-            <Link href="/listings/create">Create listing</Link>
-          </Button>
+          <SignedIn>
+            <DropdownMenu>
+              <DropdownMenuTrigger>My Account</DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuItem>
+                  <Link href={`/profile/${userId}/listings`}>Profile</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>Saved items</DropdownMenuItem>
+                <DropdownMenuItem>Orders</DropdownMenuItem>
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <Button className="bg-pink-400">
+              <Link href="/listings/create">Create listing</Link>
+            </Button>
+          </SignedIn>
         </div>
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
