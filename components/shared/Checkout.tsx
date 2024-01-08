1550493e2e01f82370e2bfab8818db079e50b355
Folka2134 <folka2134@gmail.com>
2024-01-08 20:55:03 +0000

    implemented stripe checkout page

---
+++ components/shared/Checkout.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+
+import { Button } from "../ui/button";
+import { IListing } from "@/lib/database/models/listing.model";
+import { checkoutOrder } from "@/lib/actions/order.actions";
+
+type CheckoutProps = {
+  listing: IListing;
+  userId: string;
+};
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+
+const Checkout = ({ listing, userId }: CheckoutProps) => {
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get("success")) {
+      console.log("Order placed! You will receive an email confirmation.");
+    }
+
+    if (query.get("canceled")) {
+      console.log(
+        "Order canceled -- continue to shop around and checkout when you're ready."
+      );
+    }
+  }, []);
+
+  const onCheckout = async () => {
+    const order = {
+      listingTitle: listing.title,
+      listingId: listing._id,
+      price: listing.price,
+      buyerId: userId,
+    };
+
+    await checkoutOrder(order);
+  };
+
+  return (
+    <form action={onCheckout} method="post">
+      <Button className="w-full" type="submit" role="link" size="lg">
+        Buy product
+      </Button>
+    </form>
+  );
+};
+
+export default Checkout;
