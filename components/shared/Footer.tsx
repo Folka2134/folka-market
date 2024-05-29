a5526d85a6cc0be9a553ea2be8f8ac24bd03bc75
folka2134 <folka2134@gmail.com>
2024-05-29 11:27:10 +0100

    updated bug

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -13,11 +13,11 @@ const Footer = () => {
       <div className="padding-container max-container flex w-full flex-col gap-10">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
-            {FOOTER_LINKS.map((columns) => (
-              <FooterColumn title={columns.title}>
+            {FOOTER_LINKS.map((columns, index) => (
+              <FooterColumn title={columns.title} key={index}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
-                  {columns.links.map((link) => (
-                    <Link href="/" key={link}>
+                  {columns.links.map((link, index) => (
+                    <Link href="/" key={index}>
                       {link}
                     </Link>
                   ))}
@@ -27,9 +27,9 @@ const Footer = () => {
 
             <div className="flex flex-col gap-5">
               <FooterColumn title={FOOTER_CONTACT_INFO.title}>
-                {FOOTER_CONTACT_INFO.links.map((link) => (
+                {FOOTER_CONTACT_INFO.links.map((link, index) => (
                   <div
-                    key={link.label}
+                    key={index}
                     className="flex gap-4 md:flex-col lg:flex-row"
                   >
                     <p className="whitespace-nowrap">{link.label}:</p>
